fd8228ce49fdfd2d8211c74e4ad4d1812dca9da8
Samer  El-Murr <snjmurr@gmail.com>
2023-03-29 17:51:47 +0300

    Basic setup of state management

---
+++ src/componenets/UserForm.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+interface IProps {};
+
+export const UserForm: FC<IProps> = (props) => {
+    return (
+        <div>
+            <h1>User Form</h1>
+        </div>
+    );
+}
---
+++ src/componenets/UserTable.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+interface IProps {};
+
+export const UserTable: FC<IProps> = (props) => {
+    return (
+        <div>
+            <h1>User Table</h1>
+        </div>
+    );
+}
